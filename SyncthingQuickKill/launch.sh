c31b40c17f5eafbec17c606c5f1edc7c53625d9b
XK <47260768+XK9274@users.noreply.github.com>
2024-05-15 22:22:29 +0100

    Update launch.sh

--- SyncthingQuickKill/launch.sh
+++ SyncthingQuickKill/launch.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd /mnt/SDCARD/App/Syncthing/
-export sysdir=/mnt/SDCARD/.tmp_update
-export miyoodir=/mnt/SDCARD/miyoo
-export LD_LIBRARY_PATH="/mnt/SDCARD/App/Syncthing/lib:/lib:/config/lib:$miyoodir/lib:$sysdir/lib:$sysdir/lib/parasyte"
-export PATH="$sysdir/bin:$PATH"
+
+syncthingpid() {
+	pgrep "syncthing" > /dev/null
+}
 
 if syncthingpid; then
 	killall -9 syncthing
